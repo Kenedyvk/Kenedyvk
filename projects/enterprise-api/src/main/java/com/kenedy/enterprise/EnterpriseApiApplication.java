f6a3a25e185c199d1a93990da5c06d77742e4eb8
Kenedyvk <viniciuskennedy3@gmail.com>
2026-08-31 12:28:07 -0300

    feat: add enterprise API application entrypoint

---
+++ projects/enterprise-api/src/main/java/com/kenedy/enterprise/EnterpriseApiApplication.java
@@ -0,0 +1,11 @@
+package com.kenedy.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseApiApplication.class, args);
+    }
+}
